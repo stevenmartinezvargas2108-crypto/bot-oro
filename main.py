50a4b0bf8b63aad64169ee4607ed76a1b35f1295
stevenmartinezvargas2108-crypto <stevenmartinezvargas2108@gmail.com>
2026-02-21 20:41:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,16 @@
+# -- coding: utf-8 --
 import websocket, json, ssl, time, requests
 
-# 1. Configuración
-U = "19974476"
-P = "Coste-2108"
+U, P = "19974476", "Coste-2108"
 T = "8081063984:AAGAt736SEOvD5WPQlCieD6TguIOd_MRv6s"
-C = "1417066995"
-W = "wss://ws.xtb.com/demo"
+C, W = "1417066995", "wss://ws.xtb.com/demo"
 
-print("Iniciando Bot...")
-
-# 2. Bucle de conexión
 while True:
     try:
         ws = websocket.create_connection(W, sslopt={"cert_reqs": ssl.CERT_NONE})
-        login = {"command": "login", "arguments": {"userId": U, "password": P}}
-        ws.send(json.dumps(login))
-        
-        # Si llega aquí, enviar mensaje a Telegram
-        requests.post(f"https://api.telegram.org/bot{T}/sendMessage", json={"chat_id": C, "text": "🚀 Bot Oro Online"})
-        
+        ws.send(json.dumps({"command": "login", "arguments": {"userId": U, "password": P}}))
+        requests.post(f"https://api.telegram.org/bot{T}/sendMessage", json={"chat_id": C, "text": "🚀 BOT ONLINE"})
         while True:
-            result = ws.recv()
-            print(result)
-            
-    except Exception as e:
-        print(f"Error: {e}")
+            ws.recv()
+    except:
         time.sleep(10)
