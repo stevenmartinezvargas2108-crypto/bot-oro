fb09665d222e3afff0f5046cb59c4f068111a761
stevenmartinezvargas2108-crypto <stevenmartinezvargas2108@gmail.com>
2026-02-21 20:44:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-# -- coding: utf-8 --
+# coding: utf-8
 import websocket, json, ssl, time, requests
 
-# Datos de conexión
+# Configuración
 U, P = "19974476", "Coste-2108"
 T = "8081063984:AAGAt736SEOvD5WPQlCieD6TguIOd_MRv6s"
 C, W = "1417066995", "wss://ws.xtb.com/demo"
 
 while True:
     try:
-        # Abrir conexión
+        # Conexión
         ws = websocket.create_connection(W, sslopt={"cert_reqs": ssl.CERT_NONE})
         ws.send(json.dumps({"command": "login", "arguments": {"userId": U, "password": P}}))
         
-        # Enviar aviso de encendido
-        requests.post(f"https://api.telegram.org/bot{T}/sendMessage", json={"chat_id": C, "text": "🚀 BOT ORO ONLINE"})
+        # Telegram
+        requests.post(f"https://api.telegram.org/bot{T}/sendMessage", json={"chat_id": C, "text": "🚀 BOT CONECTADO"})
         
-        # Mantener conexión viva
         while True:
             ws.recv()
             
